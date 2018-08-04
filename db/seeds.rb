d4663242328a5c4eedb01828076c06426f28e2d5
Dan Curtis <dancorpx@gmail.com>
2018-08-04 21:58:14 +0800

    initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+mock = [
+  {name: 'Mike', text: 'Johny 5 Donuts'},
+  {name: 'Dave', text: 'Daves Oasis'}
+]
+
+Story.create(mock)
+
+p "#{mock.length} stories have been added!"
